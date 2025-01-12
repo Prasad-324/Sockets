f79972aa6096d0264d8670360e7c7e3ab0df05e5
prasad <pdvprasad324@gmail.com>
2025-01-12 07:54:26 +0530

    room socket done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState,useMemo } from "react";
 import {io} from "socket.io-client";
-
+import {Container, TextField, Typography} from "@mui/material";
+import {Button} from '@mui/material';
 const  App = () => {
 
-  const socket = io("http://localhost:3000");
+  const socket = useMemo(()=>io("http://localhost:3000"),[]);
+
+  const [message,setMessage] = useState("");
+  const [room,setRoom] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message",{message,room});
+    setRoom("");
+  }
 
   useEffect(()=>{
     socket.on("connect",() =>{
       console.log("Connected", socket.id);
     })
 
+    socket.on("receive-message", (data)=> {
+      console.log(data);
+    })
+
+
     socket.on("Welcome", (s)=> {
       console.log(s);
     })
+
+    return () => {
+      socket.disconnect();
+    }
+
   },[]);
 
   return (
-    <div className="App">
-      "Hello World"
-    </div>
+  <Container maxWidth="sm">
+    <Typography>Welcome to Socket.io</Typography>
+
+    <form onSubmit={handleSubmit}>
+      <TextField value={message} onChange={ (e)=>setMessage(e.target.value)}>
+      </TextField>
+      <TextField label="Room" value={room} onChange={ (e)=>setRoom(e.target.value)}>
+      </TextField>
+      <Button type="submit">Send</Button>
+
+    </form>
+  </Container>
   );
 }
 
